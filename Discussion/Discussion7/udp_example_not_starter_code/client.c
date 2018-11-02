7a6689120d27029375e08b132b46890190994c54
Yiwen Zhang <yiwenzhg@umich.edu>
2018-11-02 10:12:17 -0400

    upload Discussion 7 pdf and udp examples

---
+++ Discussion/Discussion7/udp_example_not_starter_code/client.c
@@ -0,0 +1,63 @@
+/*
+    Simple udp client
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netdb.h>
+ 
+#define SERVER "10.0.0.1"
+#define BUFLEN 512
+#define PORT 8888
+ 
+void die(char *s)
+{
+    perror(s);
+    exit(1);
+}
+ 
+int main(void)
+{
+    struct sockaddr_in si_other;
+    int s, i, slen=sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+    sprintf(message, "HE HE");
+ 
+    if ( (s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+    {
+        die("socket");
+    }
+ 
+    memset((char *) &si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+    si_other.sin_port = htons(PORT);
+    struct hostent* sp = gethostbyname(SERVER);
+    memcpy(&si_other.sin_addr, sp->h_addr, sp->h_length);
+     
+    //if (inet_aton(SERVER , &si_other.sin_addr) == 0) 
+    //{
+    //    fprintf(stderr, "inet_aton() failed\n");
+    //    exit(1);
+    //}
+
+    //send the message
+    if (sendto(s, message, strlen(message) , 0 , (struct sockaddr *) &si_other, slen)==-1)
+    {
+        die("sendto()");
+    }
+     
+    //receive a reply and print it
+    //clear the buffer by filling null, it might have previously received data
+    memset(buf,'\0', BUFLEN);
+    //try to receive some data, this is a blocking call
+    if (recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen) == -1)
+    {
+        die("recvfrom()");
+    }
+ 
+    close(s);
+    return 0;
+}
